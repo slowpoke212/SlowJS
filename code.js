47b1077cf984f7c0572cac3b1d25c29e7f1a9954
slowpoke212 <73452171+slowpoke212@users.noreply.github.com>
2020-10-27 11:43:26 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
-document.getElementById("demo").innerHTML = "My First JavaScript";
+var level = 0;
+var mobs1 = Array("slime", "rat", "goblin", "zombie", "earth spirit");
+
+document.getElementById("demo").innerHTML = "Press C to Start Game";
+
+document.addEventListener('keydown', function(event) {
+  if (event.code == 'KeyC') {
+    document.getElementById("demo").innerHTML = "Game started";
+    mobsf()
+  }
+});
+function mobsf(){
+  var mob1 = mobs1[Math.floor(Math.random() * mobs1.length)];
+  document.getElementById("demo").innerHTML = "A " + mob1 + " apeared";
+  level += 1
+  document.getElementById("level").innerHTML = level;	
+}
