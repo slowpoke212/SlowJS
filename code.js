d67fb6be97fa23f1034a044676e60433ef27e414
slowpoke212 <73452171+slowpoke212@users.noreply.github.com>
2020-10-26 23:37:41 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,11 @@
+var button = document.createElement("button");
+button.innerHTML = "Start game";
 
+// 2. Append somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+// 3. Add event handler
+button.addEventListener ("click", function() {
+  alert("did something");
+});
